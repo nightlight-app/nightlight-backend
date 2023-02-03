48505a17f2ad1a275182b523bab475eece9d4a91
jacoblurie29 <jacoblurie29@gmail.com>
2023-02-03 00:08:51 -0600

    Fixed workflow again again

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,15 +1,19 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
+import mongoose, { ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../' });
 
 export const connectMongoDB = async () => {
-    try {
-        console.log("Connecting to MongoDB...");
-        // TODO: Ensure that you have your MongoDB URI in your .env file, then uncomment the line below
-        mongoose.connect(process.env.MONGODB_URI!);
-    } catch (err: any) {
-        console.error(err.message);
-    } finally {
-        console.log("Successfully connected to MongoDB!");
-    }
+  try {
+    console.log('Connecting to MongoDB...');
+    // TODO: Ensure that you have your MongoDB URI in your .env file, then uncomment the line below
+    mongoose.connect(process.env.MONGODB_URI!, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    } as ConnectOptions);
+  } catch (err: any) {
+    console.error(err.message);
+  } finally {
+    console.log('Successfully connected to MongoDB!');
+  }
 };
