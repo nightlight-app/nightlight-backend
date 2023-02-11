48e47c44e73524dacfffe272b1e0e39fd20c25fd
jacoblurie29 <jacoblurie29@gmail.com>
2023-02-11 00:32:34 -0600

    Venue fillEmojiCount fixed

--- src/util/venue.util.ts
+++ src/util/venue.util.ts
@@ -5,17 +5,8 @@ export interface EmojiCount {
   count: number;
 }
 
-export const fillEmojiCount = (emojiCount: EmojiCount[]) => {
-  const result = [] as EmojiCount[];
-  EMOJIS.forEach(emoji => {
-    result.push({
-      emoji,
-      count:
-        (emojiCount.find(pair => pair.emoji == emoji)
-          ? emojiCount.filter(pair => pair.emoji == emoji)[0].count
-          : 0) || 0,
-    });
-  });
-
-  return result;
+export const fillEmojiCount = (partialEmojiCount: EmojiCount[]) => {
+  let emojiCount = EMOJIS.reduce((acc, emoji) => ({ ...acc, [emoji]: 0 }), {});
+  emojiCount = { ...emojiCount, ...partialEmojiCount };
+  return emojiCount;
 };
