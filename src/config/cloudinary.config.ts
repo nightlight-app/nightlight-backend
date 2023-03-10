bdb4739addc37ee8d594cad367d2df466dca6924
Zi Nean Teoh <zinean00@gmail.com>
2023-03-09 22:36:05 -0600

    refactor to implicitly use v2

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,10 +1,10 @@
-import cloudinary from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 import multer from 'multer';
 dotenv.config();
 
 export const configureCloudinary = () => {
-  cloudinary.v2.config({
+  cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
