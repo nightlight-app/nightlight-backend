9f17662e241406c1e4b58400ce6c570c57dedc1f
jacoblurie29 <jacoblurie.dev@gmail.com>
2023-03-18 15:07:55 -0400

    Commented all mongoose models

--- src/models/Notification.model.ts
+++ src/models/Notification.model.ts
@@ -1,5 +1,17 @@
 import mongoose, { Schema } from 'mongoose';
 
+/**
+ * Notification schema
+ * @typedef NotificationSchema
+ *
+ * @type {object}
+ * @property {mongoose.Types.ObjectId} userId - User id associated with the notification.
+ * @property {string} title - The title of the notification.
+ * @property {string} [body] - The body of the notification.
+ * @property {Object} [data] - Additional data to be sent with the notification.
+ * @property {string} notificationType - The type of the notification.
+ * @property {number} [delay] - Delay in displaying the notification.
+ */
 const notificationSchema = new mongoose.Schema({
   userId: {
     type: Schema.Types.ObjectId,
