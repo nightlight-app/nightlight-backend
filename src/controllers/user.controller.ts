aa267c5293898f3cbcdfdad8760a64c363010cba
Jacob Lurie <jacoblurie.dev@gmail.com>
2023-04-19 10:51:02 -0500

    Update src/controllers/user.controller.ts
    Co-authored-by: Zi Nean Teoh <58854510+zineanteoh@users.noreply.github.com>

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -525,7 +525,7 @@ export const leaveGroup = async (req: Request, res: Response) => {
     return res.status(400).send({ message: 'No user ID provided!' });
   }
 
-  // Check if the user ID was provided
+  // Check if the group ID was provided
   if (!groupId) {
     return res.status(400).send({ message: 'No group ID provided!' });
   }
