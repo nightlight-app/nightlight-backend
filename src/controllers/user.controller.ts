d6a2e0db58792ebdb3c45fe596887d179159604e
Jacob Lurie <jacoblurie.dev@gmail.com>
2023-04-19 10:50:49 -0500

    Update src/controllers/user.controller.ts
    Co-authored-by: Zi Nean Teoh <58854510+zineanteoh@users.noreply.github.com>

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -918,7 +918,7 @@ export const removeFriendRequest = async (req: Request, res: Response) => {
     return res.status(400).send({ message: 'No user ID provided!' });
   }
 
-  // Check if the user ID was provided
+  // Check if the friend user ID was provided
   if (!friendId) {
     return res.status(400).send({ message: 'No friend user ID provided!' });
   }
